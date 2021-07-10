b9a688aa363f5ab3b5c2d829f05bc2f7c01ef4f2
IhorKov <apasniy1234@gmail.com>
2021-07-10 23:19:12 +0300

    add fix

--- src/CountString.java
+++ src/CountString.java
@@ -9,29 +9,41 @@ public class CountString {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         Map<String, Integer> map = new HashMap<>();
         String str;
-        String key = "";
-        int value = 0;
+        String keyOne = "";
+        String keyTwo = "";
+        int valueOne = 0;
+        int valueTwo = 0;
 
         while  (true){
             str = reader.readLine();
             if(str.isEmpty()) {
                 break;
             }
-            if(!map.containsKey(str)) {
-                map.put(str, 1);
+            if(map.containsKey(str)){
+                map.put(str, map.get(str) + 1);
+                continue;
             }
-            else {
-                map.put(str, (map.get(str) + 1));
+            map.put(str, 1);
+        }
+
+        for (Map.Entry<String, Integer> item: map.entrySet()){
+
+            if(item.getValue() <= valueOne && item.getValue() > valueTwo) {
+                keyTwo = item.getKey();
+                valueTwo = item.getValue();
+            }
+            else if(item.getValue() > valueOne) {
+                keyOne = item.getKey();
+                valueOne = item.getValue();
             }
         }
 
-        for (Map.Entry<String, Integer> item: map.entrySet()) {
-            if(item.getValue() > value) {
-                key = item.getKey();
-                value = item.getValue();
-            }
+        for (Map.Entry<String, Integer> item: map.entrySet()){
+            System.out.println(item.getKey() + " " + item.getValue());
         }
 
-        System.out.println(key + " " + value);
+        System.out.println();
+        System.out.println(keyOne + " " + valueOne);
+        System.out.println(keyTwo + " " + valueTwo);
     }
 }
